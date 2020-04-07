88d411b3472e740c703a4cf7f1e7d24b97331e80
Safir Salihu <msafirs@gmail.com>
2020-04-07 12:42:39 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started then, edit <code>src/App.js</code> and save to reload.
+          I was changed on the Feature branch
         </p>
       </div>
     );
